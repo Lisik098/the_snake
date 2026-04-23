9c9fec323c272ca234e50073cce2cbb060bf6eed
Алиса Гусева <al3is0a@gmail.com>
2026-04-23 21:15:36 +0300

    добавили class Apple

--- the_snake.py
+++ the_snake.py
@@ -62,6 +62,43 @@ class GameObject:
         Аргумент: surfase (поверхность, на которой рисуем)
         """
         pass
+class Apple(GameObject):
+    """
+    Класс Apple. Наследуются от GameObject.
+    Появляется в случайном месте поля.
+    """
+    apple_color = (255, 0, 0)
+
+    super().__init__(positiion=None, body_color=apple_color)
+
+    self.randomize_position()
+
+    def randomize_position(self):
+        """
+        Устанавливает координаты для яблока.
+        """
+        max_x = 640 - 20
+        max_y = 480 - 20
+
+        x.random.randrage(0, max_x + 1, 20)
+        y.random.randrage(0, max_y + 1, 20)
+
+        self.position = (x, y)
+
+    def draw(self,surface):
+        """
+        Отрисовывает яблоко на игровом поле.
+        """
+
+        rect = pygame.Rect(
+            self.position[0],
+            self.position[1],
+            20,
+            20
+        )
+        
+        pygame.draw.rect(surface, self.body_color, rect)
+
 
 
 def main():
